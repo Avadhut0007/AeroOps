18490056023902cb7b88e766dba21e66af5f7781
Avadhut <avadhutp620@gmail.com>
2024-08-20 22:20:43 +0530

    forgot password snakbar added
    conditions are checked

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginRegistrationService } from '../../services/login-registration.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-forgot-password',
@@ -10,7 +11,7 @@ import { LoginRegistrationService } from '../../services/login-registration.serv
 export class ForgotPasswordComponent implements OnInit{
   forgotPasswordForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: LoginRegistrationService) {
+  constructor(private fb: FormBuilder, private authService: LoginRegistrationService,private snakBar:MatSnackBar) {
     this.forgotPasswordForm = this.fb.group({
       emailId: ['', [Validators.required, Validators.email]]
     });
@@ -24,18 +25,31 @@ export class ForgotPasswordComponent implements OnInit{
       this.authService.sendPasswordResetLink(emailId)
         .subscribe(
           response => {
-            if (response.success) {
-              alert('Reset link sent successfully');
-            } else {
-              alert('Email ID not found');
+            if (response.status === 200) {
+              this.snakBar.open('Reset link sent successfully','Close',{
+                duration:4000,
+                verticalPosition:'top',
+                horizontalPosition:'center'
+              });
+              // alert('Reset link sent successfully');
             }
           },
           error => {
             console.error('Error from server:', error);
-            if (error.status === 200 && error.error === 'Email ID not found') {
-              alert('Email ID not found');
+            if (error.status === 500) {
+              this.snakBar.open('Email Id Not Found','Close',{
+                duration:4000,
+                verticalPosition:'top',
+                horizontalPosition:'center'
+              });
+              // alert('Email ID not found');
             } else {
-              alert('Sucess');
+              this.snakBar.open('Reset link sent successfully','Close',{
+                duration:4000,
+                verticalPosition:'top',
+                horizontalPosition:'center'
+              });
+              // alert('Sucess');
             }
           }
         );
