7234f1a101132c396a71f32f413550f57ec8ad44
Avadhut <avadhutp620@gmail.com>
2024-08-21 22:47:25 +0530

    not imp

--- src/app/components/flights/flights.component.ts
+++ src/app/components/flights/flights.component.ts
@@ -5,10 +5,6 @@ import { AfterViewInit, Component } from '@angular/core';
   templateUrl: './flights.component.html',
   styleUrl: './flights.component.css'
 })
-export class FlightsComponent implements AfterViewInit {
+export class FlightsComponent {
 
-  ngAfterViewInit(): void {
-    const airplane = document.querySelector('.airplane') as HTMLElement;
-    airplane.style.animationPlayState = 'running';
-  }
 }
